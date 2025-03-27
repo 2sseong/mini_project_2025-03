b2446406a66c04d2075de26d83a62759d702fe9e
JeongHui-seong <jgmltjd99@naver.com>
2025-03-27 13:56:44 +0900

    3일차중간

---
+++ ui/index.py
@@ -0,0 +1,308 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import QtWidgets, QtGui, uic
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt
+
+import cx_Oracle as oci
+
+sid = 'XE'
+host = '210.119.14.60'
+port = 1521
+username = 'movie'
+password = '1234'
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi('mainpage.ui',self)
+        print(widget.currentIndex())
+
+        self.btn_search.clicked.connect(self.gotoSearch)
+        self.btn_book.clicked.connect(self.gotoBookPage)
+        self.adminButton = self.findChild(QPushButton, "btn_adminlogin")
+        self.adminButton.clicked.connect(self.show_admin_login)
+
+    def show_admin_login(self):
+        dlg = LoginDialog()
+        if dlg.exec_() == QDialog.Accepted:
+
+            # 로그인 성공
+            print("관리자 로그인 성공! 관리자 페이지로 이동 가능.")
+            # 관리자 페이지 1번, 함수 만들어 안쓰고 바로 호출함
+            widget.setCurrentIndex(widget.currentIndex()+1) 
+            print(widget.currentIndex())
+        else:
+            print("관리자 로그인 취소됨")
+
+
+    def gotoSearch(self):
+        #예매조회 페이지 2번
+        widget.setCurrentIndex(widget.currentIndex()+2)
+        print(widget.currentIndex())
+    
+    def gotoBookPage(self):
+        #예매 페이지 3번
+        widget.setCurrentIndex(widget.currentIndex()+3)
+        print(widget.currentIndex())
+
+    def loginFunction(self):
+        user_id = self.user_id.text()
+        user_pw = self.user_pw.text()
+        print("성공적으로 로그인되었습니다.", user_id, user_pw)
+
+
+
+class LoginDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("admin_login.ui", self)
+
+        self.btn_ok.clicked.connect(self.check_login)
+        #self.btnCancel.clicked.connect(self.reject)
+
+    def check_login(self):
+        user_id = self.user_id.text()
+        user_pw = self.user_pw.text()
+
+        #관리자 계정 로그인
+        if user_id == "admin" and user_pw == "1234":
+            QMessageBox.information(self, "로그인 성공", "로그인에 성공했습니다!")
+            self.accept()
+        else:
+            QMessageBox.warning(self, "로그인 실패", "아이디 또는 비밀번호가 틀렸습니다.")
+
+class AdminPage(QDialog):
+    def __init__(self):
+        super(AdminPage,self).__init__()
+        loadUi('adminpage.ui', self)    
+
+        self.btn_go_to_main.clicked.connect(self.gotomain)
+    
+    def gotomain(self):
+        # 관리자 페이지 1번이므로,  홈으로 돌아오게 하려면 -1
+        widget.setCurrentIndex(widget.currentIndex()-1)
+        print(widget.currentIndex())
+
+class SearchPage(QDialog):
+    def __init__(self):
+        super(SearchPage,self).__init__()
+        loadUi('searchpage.ui',self)
+        self.btn_gotohome.clicked.connect(self.gohome)
+
+    def gohome(self):
+        widget.setCurrentIndex(widget.currentIndex()-2)
+        print(widget.currentIndex())
+
+class SearchPage(QDialog):
+    def __init__(self):
+        super(SearchPage,self).__init__()
+        loadUi('searchpage.ui',self)
+
+        self.btn_gotohome.clicked.connect(self.gohome)
+        self.btn_search.clicked.connect(self.startsearch)
+
+    def gohome(self):
+        widget.setCurrentIndex(widget.currentIndex()-2)
+        print(widget.currentIndex())
+
+    def startsearch(self):
+        std_ticket_id = self.input_key.text()
+        if std_ticket_id == '':
+            QMessageBox.warning(self, '오류', '예매번호를 입력해주세요')
+        else:
+            self.loadData(std_ticket_id) 
+
+    def loadData(self, std_ticket_id):  
+        conn = oci.connect(f'{username}/{password}@{host}:{port}/{sid}')
+        cursor = conn.cursor()
+
+        conn.begin() 
+
+        query = '''
+           SELECT user_id, name, phone_number, role_id
+             FROM gallery
+            WHERE user_id = :v_std_ticket_id
+                '''
+
+        cursor.execute(query, {'v_std_ticket_id': int(std_ticket_id)})
+
+        lst_ticket = []
+        for _, item in enumerate(cursor):
+            lst_ticket.append(item)
+        print(lst_ticket)
+        self.makeTable(lst_ticket)
+
+        cursor.close()
+        conn.close()
+
+    def makeTable(self,lst_ticket):
+            self.inplbl_1.setText(str(lst_ticket[0][0]))
+            self.inplbl_2.setText(str(lst_ticket[0][1]))
+            self.inplbl_3.setText(str(lst_ticket[0][2]))
+            self.inplbl_4.setText(str(lst_ticket[0][3]))
+
+class BookPage1(QDialog):
+    def __init__(self):
+        super(BookPage1,self).__init__()
+        loadUi('bookpage1.ui',self)
+        
+        movielist = ['영화1','영화2','영화3']
+        movietime = {1:['9:00~11:00','13:00~15:00', '17:00~19:00', '21:00~23:00'],
+                     2:['9:00~11:00','13:00~15:00', '17:00~19:00', '21:00~23:00'],
+                     3:['9:00~11:00','13:00~15:00', '17:00~19:00', '21:00~23:00']}
+
+        for i in range(len(movielist)):
+            name_movie = getattr(self, f"name_movie{i + 1}")
+            btn_movie = getattr(self,f"btn_movie{i + 1}")
+            name_movie.setText(movielist[i])
+            btn_movie.clicked.connect(lambda _, text = name_movie.text() : self.selectMovieName(text))
+
+        for i in range(len(movietime)):
+            for j in range(len(movietime[i + 1])):
+                btn_time = getattr(self,f"btn_{i + 1}time{j + 1}")
+                btn_time.setText(movietime[i + 1][j])
+                btn_time.clicked.connect(lambda _, text = btn_time.text() : self.selectTime(text))
+                btn_time.clicked.connect(self.selectTheater)
+        
+        self.btn_movie1.setIcon(QIcon('eximg.jpg'))
+        self.btn_movie1.setIconSize(self.btn_movie1.size())
+
+        self.input_moviename.textChanged.connect(self.checkInput)
+        self.input_movietime.textChanged.connect(self.checkInput)
+        self.input_theater.textChanged.connect(self.checkInput)
+
+        self.btn_next.setEnabled(False)
+        self.btn_gohome.clicked.connect(self.goHome)
+        self.btn_next.clicked.connect(self.goNext)
+
+    def selectMovieName(self, movie_name):
+        self.input_moviename.setText(movie_name)
+
+    def selectTime(self, time):
+        self.input_movietime.setText(time)
+
+    def selectTheater(self,theater):
+        sender = self.sender()
+        if sender:
+            self.input_theater.setText(sender.objectName()[4] + '관')
+        
+    def printObjectName(self):
+        sender = self.sender()
+        if sender:
+            print(sender.objectName())
+
+    def goHome(self):
+        widget.setCurrentIndex(widget.currentIndex()-3)
+        
+    def goNext(self):
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def checkInput(self):
+        input_moviename = self.input_moviename.text()
+        input_movietime = self.input_movietime.text()
+        input_theater = self.input_theater.text()
+
+        if input_moviename and input_movietime and input_theater:
+            self.btn_next.setEnabled(True)
+        else:
+            self.btn_next.setEnabled(False)
+
+class BookPage2(QDialog):
+    def __init__(self):
+        super(BookPage2,self).__init__()
+        loadUi('bookpage2.ui',self)
+
+        self.btn_next.setEnabled(False)
+
+        # if seat != '':
+        #     self.btn_next.setEnabled(True)
+
+        self.input_adt.textChanged.connect(self.checkInput)
+        self.input_teen.textChanged.connect(self.checkInput)
+
+        self.btn_goback.clicked.connect(self.goBack)
+        self.btn_next.clicked.connect(self.goNext)
+
+        for i in range(1,9):
+            btn_adt = getattr(self, f"btn_adt{i}")
+            btn_teen = getattr(self, f"btn_teen{i}")
+            btn_adt.clicked.connect(self.getAdultNumber)
+            btn_teen.clicked.connect(self.getTeenNumber)
+
+    def goBack(self):
+        widget.setCurrentIndex(widget.currentIndex()-1)
+        print(widget.currentIndex())
+
+    def goNext(self):
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def getAdultNumber(self):
+        num_adt = 0
+        btn = self.sender()
+        if btn:
+            num_adt = btn.text()
+        self.input_adt.setText(str(num_adt))
+        return num_adt
+
+    def getTeenNumber(self):
+        num_teen = 0
+        btn = self.sender()
+        if btn:
+            num_teen = btn.text()
+        self.input_teen.setText(num_teen)
+        return num_teen
+    
+    def checkInput(self):
+        adt_text = self.input_adt.text()
+        teen_text = self.input_adt.text()
+
+        if adt_text or teen_text:
+            self.btn_next.setEnabled(True)
+        else:
+            self.btn_next.setEnabled(False)
+
+class BookPage3(QDialog):
+    def __init__(self):
+        super(BookPage3,self).__init__()
+        loadUi('bookpage3.ui',self)
+
+        seat = '1'
+
+        self.btn_next.setEnabled(False)
+
+        if seat != '':
+            self.btn_next.setEnabled(True)
+
+        self.btn_goback.clicked.connect(self.goBack)
+        self.btn_next.clicked.connect(self.goNext)
+
+    def goBack(self):
+        widget.setCurrentIndex(widget.currentIndex()-1)
+        print(widget.currentIndex())
+
+    def goNext(self):
+        widget.setCurrentIndex(widget.currentIndex()+1)
+    
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = QtWidgets.QStackedWidget()
+    mainwindow = MainWindow()
+    adminPage = AdminPage()
+    searchpage = SearchPage()
+    bookpage1 = BookPage1()
+    bookpage2 = BookPage2()
+    bookpage3 = BookPage3()
+    widget.addWidget(mainwindow)
+    widget.addWidget(adminPage)
+    widget.addWidget(searchpage)
+    widget.addWidget(bookpage1)
+    widget.addWidget(bookpage2)
+    widget.addWidget(bookpage3)
+    widget.show()
+    app.exec_()
+
+    # 결제창에서 회원유무
+    # 엔터누르면 확인되게 하는거
